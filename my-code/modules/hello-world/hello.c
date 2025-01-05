97fff05668754068c51b20d9fda02c66869c5d26
David Reaver <me@davidreaver.com>
2025-01-05 12:05:22 -0800

    Add pr_fmt

--- my-code/modules/hello-world/hello.c
+++ my-code/modules/hello-world/hello.c
@@ -1,8 +1,11 @@
+// Modifies the default printk format to include the module name and function name
+#define pr_fmt(fmt) KBUILD_MODNAME ":%s:%d: " fmt, __func__, __LINE__
+
 #include <linux/module.h>
 #include <linux/kernel.h>
 
 MODULE_LICENSE("GPL");
-MODULE_AUTHOR("Your Name");
+MODULE_AUTHOR("David Reaver");
 MODULE_DESCRIPTION("A simple module example");
 
 static int __init hello_init(void)
