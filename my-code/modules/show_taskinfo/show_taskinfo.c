8ef7969d292aa356abe88fb4bb9f22e3cfc7d5fb
David Reaver <me@davidreaver.com>
2025-01-05 16:43:32 -0800

    Show info for all tasks

--- my-code/modules/show_taskinfo/show_taskinfo.c
+++ my-code/modules/show_taskinfo/show_taskinfo.c
@@ -4,6 +4,7 @@
 #include <linux/cred.h>
 #include <linux/module.h>
 #include <linux/kernel.h>
+#include <linux/sched.h>
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("David Reaver");
@@ -29,15 +30,42 @@ static int show_current_task_info(void)
 	return 0;
 }
 
+static int show_all_tasks(void)
+{
+	pr_info("------------------------------------------------------------------------------------------\n");
+	pr_info("    TGID     PID         current           stack-start         Thread Name     MT? # thrds\n");
+	pr_info("------------------------------------------------------------------------------------------\n");
+
+	struct task_struct *p, *t;
+
+	rcu_read_lock();
+	for_each_process_thread(p, t) {
+		pr_info("  %6d  %6d  0x%pK 0x%pK %s  %c  %d\n",
+			p->tgid, t->pid, t, t->stack, t->comm, task_state_to_char(t), t->signal->nr_threads);
+	}
+	rcu_read_unlock();
+
+	return 0;
+}
+
 static int __init show_taskinfo_init(void)
 {
 	pr_info("module loaded\n");
+
 	int result = show_current_task_info();
 	if (result != 0)
 	{
 		pr_err("show_current_task_info failed\n");
 		return result;
 	}
+
+	result = show_all_tasks();
+	if (result != 0)
+	{
+		pr_err("show_all_tasks failed\n");
+		return result;
+	}
+
 	return 0;
 }
 
